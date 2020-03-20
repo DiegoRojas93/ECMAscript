ac66968768432daf7c62e772e7ef329d89edb065
Diego_Fernando_Rojas_Quintero <diegorojas431@gmail.com>
2020-03-20 17:49:26 -0500

    ES7

--- src/ES6/Index.js
+++ src/ES6/Index.js
@@ -22,7 +22,7 @@ import {hello} from './module';
 
 hello();
 
-//Generadores
+//Generadores 
 
 function* helloWorld(){
     if(true){
@@ -38,3 +38,76 @@ console.log(generatorHello.next().value);
 console.log(generatorHello.next().value);
 console.log(generatorHello.next().value);
 
+        //Ejemplo 1
+
+        function* fibonacci(){
+            let index = 0;
+            var value1 = 0;
+            var value2 = 1;
+            var resultado = 0;
+            yield 0;
+            while(index < 1){
+                resultado = value1 + value2;
+                value1 = value2;
+                value2 = resultado; 
+                yield resultado;
+            }
+        }
+        const fibo = fibonacci();
+        console.log(fibo.next().value);
+        console.log(fibo.next().value);
+        console.log(fibo.next().value);
+        console.log(fibo.next().value);
+        console.log(fibo.next().value);
+        console.log(fibo.next().value);
+        console.log(fibo.next().value);
+        console.log(fibo.next().value);
+
+
+        //Ejemplo 2
+
+        function* anotherGenerator(i) {
+            yield i + 1;
+            yield i + 2;
+            yield i + 3;
+        }
+  
+        function* generator(i){
+            yield i;
+            yield* anotherGenerator(i);
+            yield i + 10;
+        }
+  
+        var gen = generator(10);
+  
+        console.log(gen.next().value); // 10
+        console.log(gen.next().value); // 11
+        console.log(gen.next().value); // 12
+        console.log(gen.next().value); // 13
+        console.log(gen.next().value); // 20
+
+
+        //Ejemplo 3
+
+        
+        function* generatorFunction(){
+            console.log('La ejecucion esta comenzando');
+      
+            yield 'Primer valor';
+
+            console.log('La ejecucion esta reiniciando');
+
+            yield 'Fin de la funcion'
+      
+        }
+
+        let dev = {};
+        const gen = generatorFunction();
+
+        dev = gen.next();
+        console.log(dev);
+        dev = gen.next();
+        console.log(dev);
+        dev = gen.next();
+        console.log(dev);
+  
---
+++ src/ES7/Index.js
@@ -0,0 +1,17 @@
+//Includes
+
+let numbers= [1, 2, 3, 6, 7]
+
+if (numbers.includes(7)) {
+    console.log('El número si se encuentra');
+}else{
+    console.log('El número no se encuentra');
+}
+
+//Exponentes
+
+let base= 4, exponente=3;
+
+let resultado= base**exponente;
+
+console.log(resultado);
