5fc14f2db9b9aa20d23b0f7b23e85691c669770f
Diego_Fernando_Rojas_Quintero <diegorojas431@gmail.com>
2020-03-21 09:50:18 -0500

    ES8

---
+++ src/ES8/Index.js
@@ -0,0 +1,53 @@
+// Object.entries
+const data = {
+    frontend: 'Oscar',
+    age: 26,
+    backend: 'Isabel',
+    design: 'Ana',
+}
+
+const list =Object.entries(data);
+console.log(list);
+
+// Object.values
+const data = {
+    frontend: 'Oscar',
+    age: 26,
+    backend: 'Isabel',
+    design: 'Ana',
+}
+
+const valores =Object.values(data);
+console.log(valores);
+
+// Object.values
+const data = {
+    frontend: 'Oscar',
+    age: 26,
+    backend: 'Isabel',
+    design: 'Ana',
+}
+
+const llaves =Object.keys(data);
+console.log(llaves);
+
+//padStar
+
+const string = 'Hello'
+
+console.log(string.padStart(8,'Hi '));
+
+
+//PadEnd
+
+const string = 'Hello';
+
+console.log(string.padEnd(11,' World'));
+
+//length
+
+const lista= [1, 'Diego', 3, 4, 5]
+
+console.log(lista.length);
+
+
